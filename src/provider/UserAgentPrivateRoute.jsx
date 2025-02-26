df3c694e5cb14871ba78a88e2203bf281d15c952
naeem majumder <naeemmajumder00@gmail.com>
2025-02-26 22:46:59 +0600

    private route

---
+++ src/provider/UserAgentPrivateRoute.jsx
@@ -0,0 +1,37 @@
+import React, { useContext, useEffect } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "./AuthProvider";
+
+const AdminPrivateRoute = ({ children }) => {
+  let { user, loading, userData, setUser, signOutUser, handleError } =
+    useContext(AuthContext); // Assuming userData contains the role
+  let location = useLocation();
+
+  useEffect(() => {
+    if ((user && user.email && userData?.role !== "Agent") && (user && user.email && userData?.role !== "User")) {
+      signOutUser()
+        .then(() => {
+          setUser(null);
+          alert("login in an ADMIN account");
+        })
+        .catch(handleError);
+    }
+  }, [user, userData]);
+
+  if (loading) {
+    return <span className="loading loading-spinner text-info"></span>;
+  }
+
+  if ((user && user.email && userData?.role === "Agent") || (user && user.email && userData?.role === "User")) {
+    return children;
+  }
+
+  return (
+    <>
+      {console.log("not an admin or not logged in")}
+      <Navigate state={location.pathname} to="/login" />
+    </>
+  );
+};
+
+export default AdminPrivateRoute;
